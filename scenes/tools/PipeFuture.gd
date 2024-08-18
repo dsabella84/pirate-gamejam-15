f75952a69d794c5ff66bb4a5f14dbd49eb7a7360
DiceWithDeath <rosieth@live.co.uk>
2024-08-18 16:49:59 +0100

    Rusty valve dialogue

--- scenes/tools/PipeFuture.gd
+++ scenes/tools/PipeFuture.gd
@@ -3,6 +3,5 @@ extends Node2D
 var dialog = load("res://scenes/dialogue/SewageValve.dialogue") as DialogueResource
 
 func _on_interactable_interacted(_initiator:Node):
-	#if WorldState.in_past: return;
-	print('1')
+	if !WorldState.in_future: return;
 	DialogState.start_dialog(dialog, "try_valve_present")
